22aab7a181a8cbde146f4e729a6f709911403945
Adelblande <adelblande@gmail.com>
2018-05-17 12:29:13 -0300

    Trabalhando com vuefire

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyDEOjhClkbFBOKw-OT4lloMfi0R0dEpm5s",
+  authDomain: "meuprojeto-bf550.firebaseapp.com",
+  databaseURL: "https://meuprojeto-bf550.firebaseio.com",
+  projectId: "meuprojeto-bf550",
+  storageBucket: "meuprojeto-bf550.appspot.com",
+  messagingSenderId: "423768631722"
+};
+const firebaseApp = firebase.initializeApp(config);
+
+export const db = firebaseApp.database();
