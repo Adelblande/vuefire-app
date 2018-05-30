bf6f509599c2ffe7c3b7134236e2e2345a82da9b
Adelblande <adelblande@gmail.com>
2018-05-30 08:31:29 -0300

    separando components

--- src/features/auth/sign-in.vue
+++ src/features/auth/sign-in.vue
@@ -1,7 +1,40 @@
 <template>
-    <div></div>
+  <form action="#" @submit.prevent="submit" class="sign-in-htm">
+    <div class="group">
+      <label for="sign-in-user" class="label">Username</label>
+      <input id="sign-in-user" type="text" class="input" v-model="username">
+    </div>
+    <div class="group">
+      <label for="sign-in-pass" class="label">Password</label>
+      <input id="sign-in-pass" type="password" class="input" data-type="password" v-model="password">
+    </div>
+    <div class="group">
+      <input id="check" type="checkbox" class="check" v-model="keepSignedin">
+      <label for="check"><span class="icon"></span> Keep me Signed in</label>
+    </div>
+    <div class="group">
+      <input type="submit" class="button" value="Sign In">
+    </div>
+    <div class="hr"></div>
+    <div class="foot-lnk">
+      <a href="#forgot">Forgot Password?</a>
+    </div>
+  </form>
 </template>
 
-<style>
-
-</style>
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      keepSignedin: true
+    }
+  },
+  methods: {
+    submit() {
+      this.$emit('do-sign-in', {...this.$data})
+    }
+  }
+}
+</script>
