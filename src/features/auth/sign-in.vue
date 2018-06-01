10a77610b316ade1b5216d11809ae4ed9a847656
Adelblande <adelblande@gmail.com>
2018-06-01 10:33:28 -0300

    Validando sign-in com Vuelidate

--- src/features/auth/sign-in.vue
+++ src/features/auth/sign-in.vue
@@ -1,12 +1,12 @@
 <template>
   <form action="#" @submit.prevent="submit" class="sign-in-htm">
     <div class="group">
-      <label for="sign-in-user" class="label">Username</label>
-      <input id="sign-in-user" type="text" class="input" v-model="username">
+      <label :class="{ invalid: $v.username.$dirty && $v.username.$invalid }" for="sign-in-user" class="label">Username</label>
+      <input :class="{ invalid: $v.username.$dirty && $v.username.$invalid }" @input="$v.username.$touch()" id="sign-in-user" type="text" class="input" v-model="username">
     </div>
     <div class="group">
-      <label for="sign-in-pass" class="label">Password</label>
-      <input id="sign-in-pass" type="password" class="input" data-type="password" v-model="password">
+      <label :class="{ invalid: $v.password.$dirty && $v.password.$invalid }" for="sign-in-pass" class="label">Password</label>
+      <input :class="{ invalid: $v.password.$dirty && $v.password.$invalid }" @input="$v.password.$touch()" id="sign-in-pass" type="password" class="input" data-type="password" v-model="password">
     </div>
     <div class="group">
       <input id="check" type="checkbox" class="check" v-model="keepSignedin">
@@ -23,6 +23,7 @@
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators'
 export default {
   data() {
     return {
@@ -31,18 +32,33 @@ export default {
       keepSignedin: true
     }
   },
+  validations: {
+    username: {
+      required,
+    },
+    
+    password: {
+      required,
+    },
+
+  },
   mounted() {
     this.$bus.$on('navigate', this.reset)
   },
   methods: {
     submit() {
-      this.$emit('do-sign-in', {...this.$data})
+      if(!this.$v.$invalid){
+        this.$emit('do-sign-in', {...this.$data})
+      }else{
+        this.$v.$touch()
+      }
     },
     reset(selected){
       if(selected == 'signup'){
         this.username = '',
         this.password = '',
-        this.keepSignedin = true
+        this.keepSignedin = true,
+        this.$v.$reset()
       }
     }
   }
